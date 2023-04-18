1a2fa234af52aff7e9037d57a0886e6886f1ca47
Haziq Kamel <haziqkamel@outlook.com>
2023-04-18 10:25:54 +0800

    fix: action.js

--- simplepeer-webrtc-app/src/store/action.js
+++ simplepeer-webrtc-app/src/store/action.js
@@ -1,6 +1,8 @@
 const Actions = {
     SET_IS_ROOM_HOST: "SET_IS_ROOM_HOST",
     SET_CONNECT_ONLY_WITH_AUDIO: "SET_CONNECT_ONLY_WITH_AUDIO",
+    SET_ROOM_ID: "SET_ROOM_ID",
+    SET_IDENTITY: "SET_IDENTITY",
 }
 
 export const setIsRoomHost = (isRoomHost) => {
