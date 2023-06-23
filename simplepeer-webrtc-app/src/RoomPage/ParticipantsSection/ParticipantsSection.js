8b715a6691fbff984112f669349ab1faee6bed95
Haziq Kamel <haziqkamel@live.com>
2023-06-23 15:05:14 +0800

    creating direct message container

--- simplepeer-webrtc-app/src/RoomPage/ParticipantsSection/ParticipantsSection.js
+++ simplepeer-webrtc-app/src/RoomPage/ParticipantsSection/ParticipantsSection.js
@@ -1,15 +1,16 @@
-import React from "react"
-import ParticipantsLabel from "./ParticipantsLabel"
-import Participants from "./Participants"
+import React from "react";
+import ParticipantsLabel from "./ParticipantsLabel";
+import Participants from "./Participants";
+import DirectChat from "./DirectChat/DirectChat";
 
 const ParticipantsSection = () => {
+  return (
+    <div className="participants_section_container">
+      <ParticipantsLabel />
+      <Participants />
+      <DirectChat />
+    </div>
+  );
+};
 
-    return (
-        <div className="participants_section_container">
-            <ParticipantsLabel />
-            <Participants />
-        </div>
-    )
-}
-
-export default ParticipantsSection
+export default ParticipantsSection;
